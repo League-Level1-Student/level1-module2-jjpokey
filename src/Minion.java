529e1bb317296b5c550fd0545f2e48dc962d1de3
Sofisa <league@WTS10.attlocal.net>
2018-10-15 16:53:23 -0700

    Level1m3

--- src/Minion.java
+++ src/Minion.java
@@ -4,6 +4,7 @@ public class Minion {
 		private int eyes; 
 		private String color; 
 		private String master;
+		
 Minion(String name, int eyes, String color, String master) {
 this.name = name;
 this.eyes = eyes;
